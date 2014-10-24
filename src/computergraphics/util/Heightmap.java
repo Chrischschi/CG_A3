efc22b4f522126e1b33f3b98470be1954968a487
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-10-24 17:00:39 +0200

    Angefangen mit Aufgabe 2
    Methode Heightfield.applyHeightValues mit JavaDoc erstellt, muss noch
    implementiert werden.

--- src/computergraphics/util/Heightmap.java
+++ src/computergraphics/util/Heightmap.java
@@ -9,7 +9,18 @@ public class Heightmap {
      * @return 
      */
     public static ITriangleMesh create() {
+        //TODO hier applyHeightValues(ITriangleMesh) aufrufen
         return null; // TODO Implementieren und parameter bestimmen
+    }
+    
+    /** Setzt auf einer als dreiecksnetz dargestellten gitterstruktur die aus 
+     * einer datei ausgelesenen y-werte für ein höhenfeld.   
+     * @param lattice Das dreiecksnetz dessen y-werte verändert werden sollen
+     * @param maxHeightValue Der y-wert, den die Farbe Weiß aus dem Bild mit den
+     * höhendaten darstellen soll. Dies ist auch der maximale y-wert 
+     */
+    private static void applyHeightValues(ITriangleMesh lattice,double maxHeightValue) {
+        //TODO Festlegen, wie man das Bild mit den höhen an die methode übergibt
     }
 
 }
